d0ed4fb3f23aa0a18150a53d7b8798b5440a97a1
mkmulvi30 <98337534+mkmulvi30@users.noreply.github.com>
2022-06-15 11:43:01 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,4 +2,11 @@ let value = 0
 basic.forever(function () {
     value = pins.analogReadPin(AnalogPin.P0)
     basic.showNumber(value)
+    if (pins.analogReadPin(AnalogPin.P0) < 600) {
+        basic.showIcon(IconNames.Happy)
+    } else if (pins.analogReadPin(AnalogPin.P0) > 600 && pins.analogReadPin(AnalogPin.P0) < 850) {
+        basic.showIcon(IconNames.Surprised)
+    } else {
+        basic.showIcon(IconNames.Asleep)
+    }
 })
